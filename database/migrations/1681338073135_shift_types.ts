3a8180ea00a2d0ed25ba9ac5a82d8460071a155a
Ron Tseytlin <ronts390@gmail.com>
2023-04-13 16:38:11 +0300

    formatted

--- database/migrations/1681338073135_shift_types.ts
+++ database/migrations/1681338073135_shift_types.ts
@@ -3,7 +3,7 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'shift_types'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.integer('schedule_id').unsigned().references('schedules.id')
@@ -17,7 +17,7 @@ export default class extends BaseSchema {
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
--- database/migrations/1681342526875_shifts.ts
+++ database/migrations/1681342526875_shifts.ts
@@ -3,7 +3,7 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'shifts'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.integer('shift_type_id').unsigned().references('shift_types.id')
       table.integer('member_id').unsigned().references('members.id')
@@ -18,7 +18,7 @@ export default class extends BaseSchema {
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
